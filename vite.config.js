729f75175ac3512dbfe20f93823f379d1bf0bc09
Ed <caceresrivera321@gmail.com>
2025-11-30 14:12:30 -0600

    4
    ...

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(
+  {
   plugins: [
+    
     tailwindcss(),
     react()
   ],
+  base: process.env.DEPLOY_BASE_URL,
 })
